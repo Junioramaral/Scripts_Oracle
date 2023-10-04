94eca808b55db5e4db067db48833f3c910e84532
Junior Amaral <junior.amaral.poa@gmail.com>
2023-10-04 19:34:39 -0300

    Script: metrica_memory_pdbs.sql -> Mostra as Metricas de Memoria dos PDB's

---
+++ metrica_memory_pdbs.sql
@@ -0,0 +1,6 @@
+-- A viewV$RSRCPDBMETRIC disponibiliza para nós as informações da quantidade de memória usada pelos PDBs.
+
+select r.con_id, p.pdb_name, r.sga_bytes, r.pga_bytes, r.buffer_cache_bytes, r.shared_pool_bytes
+ from V_$RSRCPDBMETRIC r, cdb_pdbs p
+where r.con_id = p.con_id
+/
